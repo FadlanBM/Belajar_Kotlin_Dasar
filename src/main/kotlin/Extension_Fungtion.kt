e03e760d9073da3174dca814c01a5a4d7602eeee
FadlanBM <fadlankurahan2@gmail.com>
2022-12-20 10:57:34 +0700

    Mengedit Exstension Fungtion

--- src/main/kotlin/Extension_Fungtion.kt
+++ src/main/kotlin/Extension_Fungtion.kt
@@ -1,3 +1,13 @@
-fun main() {
+fun String.anjay()="Selamat Datang $this"
+fun String.sapa()= println("Terimakasih Atas Kunjungannya $this")
+fun Int.hitunga():Int=this+10
 
+
+fun main() {
+    val name="Fadlan"
+    println(name.anjay())
+    "Fadlan Buwono Mukti".sapa()
+    "Kuntul".sapa()
+    var jajal=10.hitunga()
+    println("Hasil adalah $jajal")
 }
